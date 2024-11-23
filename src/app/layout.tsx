835861610b493a52665b161ee5e7d0820ac3834e
Tjcarroll1 <tjcarroll1@icloud.com>
2024-11-23 11:59:15 +1000

    adding google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,16 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {/* eslint-disable-next-line */}
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PNZNDMHP');`}
+        </Script>
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
@@ -36,6 +46,15 @@ export default function RootLayout({
         <script src="https://assets.calendly.com/assets/external/widget.js" async></script>
       </head>
       <body className="flex min-h-screen flex-col font-sans">
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PNZNDMHP"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
